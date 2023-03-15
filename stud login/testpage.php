49cefcb7ec137e2478c414360165d76a7b0b0fc1
Vishal Hanchnoli <hanchnoli@gmail.com>
2023-03-15 21:30:32 +0530

    Update testpage.php

--- stud login/testpage.php
+++ stud login/testpage.php
@@ -27,7 +27,7 @@ else{
 }
 ////////////////////
 
-$conn = mysqli_connect("sql208.hyperphp.com","hp_32984812","2f3fccd3b1d","hp_32984812_mcqtest");
+$conn = mysqli_connect("");
 if(!$conn)
 	die("FAILED TO CONNECT".mysqli_connect_error($conn));
 
