3d1a3dcb93120b60b8a432f3359e55a7a9742d36
Vishal Hanchnoli <hanchnoli@gmail.com>
2023-03-15 21:31:33 +0530

    Update deletetest.php

--- fac login/deletetest.php
+++ fac login/deletetest.php
@@ -1,5 +1,5 @@
 <?php session_start(); date_default_timezone_set('Asia/Kolkata');
-$conn = mysqli_connect("sql208.hyperphp.com","hp_32984812","2f3fccd3b1d","hp_32984812_mcqtest");
+$conn = mysqli_connect("");
 if(!$conn)
 	die(mysqli_connect_error($conn));
 ?>
